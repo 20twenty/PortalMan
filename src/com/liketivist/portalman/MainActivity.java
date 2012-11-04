1b99d8fcd66bfb935d60419736b46fb6e9db6f49
20twenty <brady_benware@hotmail.com>
2012-11-03 21:40:48 -0700

    added game panel and main state thread

--- src/com/liketivist/portalman/MainActivity.java
+++ src/com/liketivist/portalman/MainActivity.java
@@ -2,14 +2,44 @@ package com.liketivist.portalman;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+   private static final String TAG = "MainActivity";
+
+   /** Called when the activity is first created. */
+   @Override
+   public void onCreate(Bundle savedInstanceState)
+   {
+      super.onCreate(savedInstanceState);
+      requestWindowFeature(Window.FEATURE_NO_TITLE);
+      getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+      setContentView(new MainGamePanel(this));
+      Log.d(TAG, "onCreate");
+      //setContentView(R.layout.main);
+   }
+
+   @Override
+   protected void onDestroy() {
+      // TODO Auto-generated method stub
+      super.onDestroy();
+      Log.d(TAG, "onDestroy");
+   }
+
+   @Override
+   protected void onPause() {
+      // TODO Auto-generated method stub
+      super.onPause();
+      Log.d(TAG, "onPause");
+   }
+
+   @Override
+   protected void onResume() {
+      // TODO Auto-generated method stub
+      super.onResume();
+      Log.d(TAG, "onResume");
+   }
 }
